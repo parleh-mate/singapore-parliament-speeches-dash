0823235fbb8241a33711382f6480d01376d0692e
jonfoong <jonahfoong@gmail.com>
2024-10-09 20:21:23 +0200

    cosmetic changes; add blurring and customize toggle icon

--- pages/home/__init__.py
+++ pages/home/__init__.py
@@ -1,33 +1,58 @@
 from dash import html
 import dash_bootstrap_components as dbc
+from dash_iconify import DashIconify
 
 # Home page layout
 home_page = html.Div(
     [
-        html.H1("Welcome to the Parliamentary Analysis App"),
-        html.P("Explore parliamentary speeches and topics."),
+        html.H2("Find out what's going on in the SG Parliament!"),
     ]
 )
 
 # Navbar
+import dash_bootstrap_components as dbc
+from dash import html
+
 navbar = dbc.Navbar(
     [
-        dbc.Button(html.Span(className="navbar-toggler-icon"), color="primary", className="me-2 d-md-none",
-                   id="sidebar-toggle", n_clicks=0),
-        dbc.NavbarBrand("Parliamentary Analysis", className="ms-2"),
+        dbc.Button(
+            DashIconify(icon="mdi:menu", width=24, height=24),  # Set desired icon size
+            color="primary",
+            className="me-2 d-md-none",
+            id="sidebar-toggle",
+            n_clicks=0,
+            style={
+                "border": "none",                               # Remove border
+                "padding": "0",                                 # Remove padding
+                "width": "40px",                                # Set fixed width
+                "height": "40px",                               # Set fixed height
+                "display": "flex",                              # Use flex to center the icon
+                "alignItems": "center",                         # Vertically center
+                "justifyContent": "center",                     # Horizontally center
+            }
+        ),
+        dbc.NavbarBrand("Parleh-mate!", className="ms-2"),
     ],
     color="light",
     dark=False,
     sticky="top",
+    className="custom-navbar"  # Apply the custom CSS class
 )
+
 
 # Sidebar content
 sidebar_content = [
     dbc.Nav(
         [
             dbc.NavLink("Parleh-mate", href="/", active="exact"),
-            dbc.NavLink("Speeches", href="/speeches", active="exact"),
-            # Removed Page 2 Link
+            dbc.NavLink("Participation", href="/attendance", active="exact"),
+            dbc.NavLink("Speeches", href="/speeches", active="exact"), 
+            dbc.NavLink("Bills", href="/bills", active="exact"),
+            dbc.NavLink("Topics", href="/topics", active="exact"), 
+            dbc.NavLink("Questions", href="/questions", active="exact"),
+            dbc.NavLink("Demographics", href="/demographics", active="exact"),
+            dbc.NavLink("Methodology", href="/methodology", active="exact"),  
+            dbc.NavLink("About", href="/about", active="exact"),          
         ],
         vertical=True,
         pills=True,
