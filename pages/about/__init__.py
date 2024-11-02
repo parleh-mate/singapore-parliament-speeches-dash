084cd1218d8d7fc09b1d3c96ec26ee1ba8f924e8
jonfoong <jonahfoong@gmail.com>
2024-11-02 22:27:44 +0100

    expand about section

--- pages/about/__init__.py
+++ pages/about/__init__.py
@@ -1,13 +1,30 @@
 from dash import html
+import dash_bootstrap_components as dbc
 
 def about_layout():
-    return html.Div(
+    return dbc.Container(
         [
-            html.H1("About"),
-            html.P(
-                "Some about stuff."
-            )
+            dbc.Row(
+                dbc.Col(
+                    [
+                        html.H1("About"),
+                        html.P(
+                            "Par-leh-mate was created with the intention of shedding light on the goings-on in the Singapore Parliament. It is intended as a tool for citizens to understand better how their MPs represent their interests and is not intended to be political in nature."
+                            ),
+                        html.Br(),
+                        html.Br(),
+                        html.P("Please send email inquiries to xx. For more information visit the Github page: xx")
+                    ],
+                    width=10  # Occupy 10 out of 12 columns (~83.33% width)
+                ),
+                justify="center"  # Center the column within the row
+            ),
         ],
-        className='content',
+        fluid=True,  # Ensures the container spans the full width of the viewport
         style={'padding': '20px'}
     )
+
+
+
+
+            
